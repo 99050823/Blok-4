401fa1389524b11508bb0e2286b618a47697ffd8
99050823 <99050823@mydavinci.nl>
2022-03-24 14:04:24 +0100

    Commit - TO-DO List : Basic HTML Markup
    - Added an index file with all basic elements needed in the project.

---
+++ To-Do - Project/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo list</title>
+</head>
+<body>
+
+    <div class="container">
+        <header>
+            <div>
+                <h2>Username: User</h2>
+            </div>
+
+            <div>
+                <ul>
+                    <li>item1</li>
+                    <li>item2</li>
+                    <li>item3</li>
+                    <li>item4</li>
+                </ul>
+            </div>
+        </header>
+
+        <section>
+            <!-- LIST Elements -->
+
+            <div data-todo="list">
+                <h3>List Title</h3>
+                <button>Delete</button>
+
+                <ul>
+                    <li>Todo1</li>
+                    <li>Todo1</li>
+                    <li>Todo1</li>
+                </ul>
+
+                <p>Username</p>
+                <button>View Button</button>
+            </div>
+
+            <button>Add List</button>
+        </section>
+
+        <footer>Footer</footer>
+    </div>
+
+</body>
+</html>
